8f157102632e297f1eb9848377e0a5f4f4dc911f
Ianne <bulilaniannecarl@gmail.com>
2025-03-14 18:32:04 +0800

    added tab icon and tab title

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <SessionProvider><Component {...pageProps} /></SessionProvider>;
+  return (
+  <SessionProvider>
+    <Head>
+      <link rel="icon" href="/logo.png" />
+      <title>DepHelp</title>
+    </Head>
+    <Component {...pageProps} />
+    
+    </SessionProvider>
+    )
 }
